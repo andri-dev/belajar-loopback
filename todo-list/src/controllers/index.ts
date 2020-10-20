2bf833d3cd9552665c7abae57c2be725db54ef2f
andrijunaedi <andrijunaedi126@gmail.com>
2020-10-20 16:22:34 +0700

    todo-list

---
+++ todo-list/src/controllers/index.ts
@@ -0,0 +1,2 @@
+export * from './todo.controller';
+export * from './profile.controller';
---
+++ todo-list/src/models/profile.model.ts
@@ -0,0 +1,39 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Profile extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstName: string;
+
+  @property({
+    type: 'string',
+  })
+  lastName?: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  age: number;
+
+
+  constructor(data?: Partial<Profile>) {
+    super(data);
+  }
+}
+
+export interface ProfileRelations {
+  // describe navigational properties here
+}
+
+export type ProfileWithRelations = Profile & ProfileRelations;
